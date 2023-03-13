3bac7223700784e3bbc14fd83e06d086e5cf4906
kitez <duswl339@ajou.ac.kr>
2023-03-13 10:45:48 +0900

    코드 주석 처리된 부분 수정

--- YEON/BAEKJOON/1049.js
+++ YEON/BAEKJOON/1049.js
@@ -16,5 +16,6 @@ let packagePay = Math.floor(N/6);
 // packages 가격 중 최솟값이 singles 최솟값 * 6 보다 큰 경우 낱개로 사는게 이득
 cost += Math.min(...packages) > Math.min(...singles) * 6 ? Math.min(...singles) * packagePay * 6 : Math.min(...packages) * packagePay;
 
-// 낱개 최솟값 * 남은 구매 수 보다 패키지 최솟값으로 구매하는 것이 더 싼 경우 패키지 구매가 이득득cost += Math.min(...singles) * (N - (packagePay) * 6) < Math.min(...packages) ? Math.min(...singles) * (N - (packagePay) * 6) : Math.min(...packages)
+// 낱개 최솟값 * 남은 구매 수 보다 패키지 최솟값으로 구매하는 것이 더 싼 경우 패키지 구매가 이득
+cost += Math.min(...singles) * (N - (packagePay) * 6) < Math.min(...packages) ? Math.min(...singles) * (N - (packagePay) * 6) : Math.min(...packages)
 console.log(cost);
