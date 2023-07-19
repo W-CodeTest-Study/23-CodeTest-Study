07f79afdbcd04b8f27a1d45d42e521f9c9ff680b
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-07-19 18:30:09 +0900

    [week3] hcmhcs:p42626(주석추가)

--- Hcmins/p42626.py
+++ Hcmins/p42626.py
@@ -2,11 +2,12 @@ import heapq
 
 
 def solution(scoville, K):
+    # heap자료구조로 변환
     heapq.heapify(scoville)
     count = 0
-    while True:
-        if len(scoville) < 2:
-            break
+
+    while len(scoville) > 1:
+        # 최소 뽑아서 K보다 작은지 확인 후 작으면 공식대로 합쳐서 힙에 다시 넣기
         min = heapq.heappop(scoville)
         if min < K:
             min_second = heapq.heappop(scoville)
@@ -14,7 +15,9 @@ def solution(scoville, K):
             count += 1
         else:
             break
+    # 만들수 없을 때 -1 return
     if heapq.heappop(scoville) < K:
         return -1
+    # 만들수 있으면 횟수 return
     else:
         return count
