e35b1d0975fd58ed9fd60031cbc3c64d77f1c564
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-07-19 18:24:58 +0900

    [week3] hcmhcs:p42747(주석추가)

--- Hcmins/p42587.py
+++ Hcmins/p42587.py
@@ -2,20 +2,22 @@ from collections import deque
 
 
 def solution(priorities, location):
-    prior = deque(priorities)
-    index = deque(list(i for i in range(len(priorities))))
+    prior = deque(priorities)  # 우선순위 큐
+    index = deque(list(i for i in range(len(priorities))))  # 프로세스의 index 큐
     answers = []
 
     while len(prior) != 0:
         max_process = max(prior)
         one_prior = prior.popleft()
         one_index = index.popleft()
+        # 가장 큰 우선순위이면 pop해주고 그 인덱스를 answers에 저장
         if (one_prior == max_process):
             answers.append(one_index)
             continue
+        # 더 큰 우선순위가 있으면 다시 큐에 넣기
         else:
             prior.append(one_prior)
             index.append(one_index)
-
+    # 현재 먼저 실행된(pop된) 프로세스 순으로 answers index가 담겨있으니까 원하는 index의 순서를 확인해서 return
     answer = answers.index(location)+1
     return answer
