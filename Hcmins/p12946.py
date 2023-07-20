1f28638253e4a564c98073e10f4e04fca919c32a
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-07-20 20:04:34 +0900

    [week3] hcmhcs:p12946(코드수 줄임)

--- Hcmins/p12946.py
+++ Hcmins/p12946.py
@@ -1,12 +1,7 @@
-def mov(src, des, answer):
-    answer.append([src, des])
-    return 0
-
-
 def recur(n, current, target, answer):  # n:남은 수,current:현재 위치, target:이동한 위치, answer:경로저장용
     middle = 6-current-target  # 중간에 있는 애
     if (n == 1):
-        mov(current, target, answer)
+        answer.append([current, target])  # 옮긴거 저장
     if (n > 1):
         recur(n-1, current, middle, answer)  # 아래집합들 중간에 넣고
         recur(1, current, target, answer)  # 맨아래를 target으로 옮기고
