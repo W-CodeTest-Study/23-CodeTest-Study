3ae74cf92f07a892b369f0f674c4741bf97f2580
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-08-10 22:34:54 +0900

    hcmhcs: p10610(최애숫자 30)-되풀이

--- Hcmins/p10610.py
+++ Hcmins/p10610.py
@@ -1,6 +1,25 @@
-from itertools import permutations
-N = input()
+# from itertools import permutations
+# N = input()
 
+# arr = list(map(int, str(N)))
+
+# arr.sort(key=lambda x: -x)
+# # 30의 배수가 아닌 조건 확인
+# if arr[len(arr)-1] != 0 or sum(arr) % 3 != 0:
+#     print(-1)
+# # 3의 배수를 만들 수 있는데 그 값을 찾기
+# else:
+#     arr.remove(0)
+#     permute = list(permutations(arr))
+
+#     for i in range(len(permute)):
+#         num = ''.join(map(str, list(permute[i])))
+#         if int(num) % 3 == 0:
+#             print(num+'0')
+#             break
+
+# 메모리 초과로 풀이참조 -> 수학;; 합이 3의 배수면 어떤 조합을 해도 3의배수라네요
+N = input()
 arr = list(map(int, str(N)))
 
 arr.sort(key=lambda x: -x)
@@ -9,11 +28,5 @@ if arr[len(arr)-1] != 0 or sum(arr) % 3 != 0:
     print(-1)
 # 3의 배수를 만들 수 있는데 그 값을 찾기
 else:
-    arr.remove(0)
-    permute = list(permutations(arr))
-
-    for i in range(len(permute)):
-        num = ''.join(map(str, list(permute[i])))
-        if int(num) % 3 == 0:
-            print(num+'0')
-            break
+    num = ''.join(map(str, arr))
+    print(num)
