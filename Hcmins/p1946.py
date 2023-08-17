4c4af80c39e702046b23dc3050851ec68c2d0502
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-08-17 21:07:30 +0900

    hcmhcs: p1946(방법3 , but 시간초과)

--- Hcmins/p1946.py
+++ Hcmins/p1946.py
@@ -1,33 +1,72 @@
+# T = int(input())
+# N = []
+# all_rank = []
+# start_point = 0
+# for i in range(T):
+#     num = int(input())
+#     N.append(num)
+#     for j in range(num):
+#         all_rank.append(list(map(int, input().split())))
+# def check(start_point, num): # 시작포인트랑 몇개까지인지
+#     answer = num
+#     rank = all_rank[start_point:start_point+num]
+#     rank.sort()  # 정렬해서 서류결과비교는 스킵
+#     for i in range(num):
+#         opt = 0
+#         one = rank[i]  # 탈락인지 아닌지 확인할 대상
+#         for j in range(i):
+#             # 앞에 애들과 면접서류 비교하고 작은게 있으면 opt=1로 바꿈(탈락자라는 의미)
+#             if (rank[j][1] < one[1]):
+#                 opt = 1
+#                 break
+#         if (opt == 1):
+#             answer -= 1  # 탈락할때마다 전체인원에서 뺌
+#     return answer
+# for i in range(T):
+#     print(check(start_point, N[i]))
+#     start_point += N[i]
+
+
+# 방법2: 반복문 돌리면서 최고순위 임시변수에 저장해두고, but 시간초과
+# T = int(input())
+# N = []
+# all_rank = []
+# start_point = 0
+# for i in range(T):
+#     num = int(input())
+#     N.append(num)
+#     for j in range(num):
+#         all_rank.append(list(map(int, input().split())))
+# def check(start_point, num) -> int:
+#     answer = 1
+#     rank = all_rank[start_point:start_point+num]
+#     rank.sort()  # 정렬해서 서류결과비교는 스킵
+#     temp = int(rank[0][1])
+#     for i in range(1, num):
+#         one = rank[i]  # 탈락인지 아닌지 확인할 대상
+#         if one[1] < temp:
+#             answer += 1
+#             temp = one[1]
+#     return answer
+# for i in range(T):
+#     print(check(start_point, N[i]))
+#     start_point += N[i]
+
+
 T = int(input())
-N = []
-all_rank = []
-start_point = 0
+answer = []
 for i in range(T):
+    rank = []
     num = int(input())
-    N.append(num)
     for j in range(num):
-        all_rank.append(list(map(int, input().split())))
-
-# 시작포인트랑 몇개까지인지
-
-
-def check(start_point, num):
-    answer = num
-    rank = all_rank[start_point:start_point+num]
-    rank.sort()  # 정렬해서 서류결과비교는 스킵
-    for i in range(num):
-        opt = 0
-        one = rank[i]  # 탈락인지 아닌지 확인할 대상
-        for j in range(i):
-            # 앞에 애들과 면접서류 비교하고 작은게 있으면 opt=1로 바꿈(탈락자라는 의미)
-            if (rank[j][1] < one[1]):
-                opt = 1
-                break
-        if (opt == 1):
-            answer -= 1  # 탈락할때마다 전체인원에서 뺌
-    return answer
-
-
+        rank.append(list(map(int, input().split())))
+    rank.sort(key=lambda x: x[0])
+    count = 1
+    temp = rank[0][1]
+    for j in range(1, num):
+        if rank[j][1] < temp:
+            count += 1
+            temp = rank[j][1]
+    answer.append(count)
 for i in range(T):
-    print(check(start_point, N[i]))
-    start_point += N[i]
+    print(answer[i])
