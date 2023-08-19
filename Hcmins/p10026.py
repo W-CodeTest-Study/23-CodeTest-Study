83dd89084d6c7195cc202905ddb9d9d2fcd8027b
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-08-19 16:16:29 +0900

    hcmhcs: p10026:진행중

---
+++ Hcmins/p10026.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = []
+for i in range(N):
+    str = input()
+    arr.append(list(str))
+
+print(arr)
+answer1 = 0
+answer2 = 0
+
+
+def normal() -> int:
+    return 0
+
+
+def blind() -> int:
+    return 0
