b0deb95ecb3b386558b0b97356225e7967e93ceb
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-08-20 03:29:36 +0900

    hcmhcs: p10026(시간초과,구현완료)

--- Hcmins/p10026.py
+++ Hcmins/p10026.py
@@ -1,21 +1,54 @@
 N = int(input())
-arr = []
+map = []
+answer1, answer2 = 0, 0
 for i in range(N):
     str = input()
-    arr.append(list(str))
-
-print(arr)
-answer1 = 0
-answer2 = 0
-
-# return : 일반인이 볼때 갯수
+    map.append(list(str))
+# 상하좌우보면서 같은색은 stack에서 제거해준다.
 
 
-def normal() -> int:
-    return 0
+def count(x, y, color):
+    s = [x, y+1]
+    n = [x, y-1]
+    e = [x+1, y]
+    w = [x-1, y]
+    if s in stack and map[s[0]][s[1]] == color:
+        stack.remove(s)
+        count(s[0], s[1], color)
+    if n in stack and map[n[0]][n[1]] == color:
+        stack.remove(n)
+        count(n[0], n[1], color)
+    if e in stack and map[e[0]][e[1]] == color:
+        stack.remove(e)
+        count(e[0], e[1], color)
+    if w in stack and map[w[0]][w[1]] == color:
+        stack.remove(w)
+        count(w[0], w[1], color)
+    return 1
 
-# return : 적록색약이 볼때 갯수
 
+# 정상일 경우
+stack = []
+for i in range(N):
+    for j in range(N):
+        stack.append([i, j])
+color = map[0][0]
+while stack:
+    point = stack.pop()
+    x, y = point[0], point[1]
+    color = map[x][y]
+    answer1 += count(x, y, color)
+# 비정상일경우
+for i in range(N):
+    for j in range(N):
+        stack.append([i, j])
+        if map[i][j] == 'G':
+            map[i][j] = 'R'
+color = map[0][0]
+while stack:
+    point = stack.pop()
+    x, y = point[0], point[1]
+    color = map[x][y]
+    answer2 += count(x, y, color)
 
-def blind() -> int:
-    return 0
+print(answer1, answer2)
